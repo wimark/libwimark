4d11a9a86911ac5ca4040dd9b37730ae0474e4a2
Artem Vorotnikov <artem@vorotnikov.me>
2017-02-17 09:32:19 +0300

    Fixed topic parsing tests

--- util_test.go
+++ util_test.go
@@ -14,28 +14,28 @@ const (
 )
 
 func TestParseRequest(t *testing.T) {
-	var fixture = ParseTopic("U/CONFIG/A001/DB/B002/REQ/G45134513/R")
+	var fixture = "U/CONFIG/A001/DB/B002/REQ/G45134513/R"
 	var expectation = fmt.Sprintf(REQ_JSON_FORMAT, DirectionUnicast, ModuleConfig, "A001", ModuleDB, "B002", MessageRequest, "G45134513", OperationRead)
-	var j, _ = json.Marshal(fixture)
+	var j, _ = json.Marshal(ParseTopic(fixture))
 	var result = string(j)
 
-	assert.Equal(t, result, expectation)
+	assert.Equal(t, expectation, result)
 }
 
 func TestParseResponse(t *testing.T) {
-	var fixture = ParseTopic("U/CONFIG/A001/DB/B002/RSP/G45134513")
+	var fixture = "U/CONFIG/A001/DB/B002/RSP/G45134513"
 	var expectation = fmt.Sprintf(RSP_JSON_FORMAT, DirectionUnicast, ModuleConfig, "A001", ModuleDB, "B002", MessageResponse, "G45134513")
-	var j, _ = json.Marshal(fixture)
+	var j, _ = json.Marshal(ParseTopic(fixture))
 	var result = string(j)
 
-	assert.Equal(t, result, expectation)
+	assert.Equal(t, expectation, result)
 }
 
-func TestBroadcast(t *testing.T) {
-	var fixture = ParseTopic("B/CPE/AP001")
+func TestParseBroadcast(t *testing.T) {
+	var fixture = "B/CPE/AP001"
 	var expectation = fmt.Sprintf(B_JSON_FORMAT, DirectionBroadcast, ModuleCPE, "AP001")
-	var j, _ = json.Marshal(fixture)
+	var j, _ = json.Marshal(ParseTopic(fixture))
 	var result = string(j)
 
-	assert.Equal(t, result, expectation)
+	assert.Equal(t, expectation, result)
 }
