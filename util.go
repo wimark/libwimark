dd2af6069b7b610dbd6c6f51f5038ad0d8191ebb
Artem Vorotnikov <artem@vorotnikov.me>
2017-02-17 09:31:33 +0300

    Simplified string handling

--- util.go
+++ util.go
@@ -32,18 +32,15 @@ func (self module) toString() string {
 	return v
 }
 
-func parseModuleString(v_p *string) *module {
-	if v_p != nil {
-		var v = *v_p
-		var ret, ok = map[string]module{
-			"BACKEND": ModuleBackend,
-			"CONFIG":  ModuleConfig,
-			"DB":      ModuleDB,
-			"CPE":     ModuleCPE,
-		}[v]
-		if ok {
-			return &ret
-		}
+func parseModuleString(v string) *module {
+	var ret, ok = map[string]module{
+		"BACKEND": ModuleBackend,
+		"CONFIG":  ModuleConfig,
+		"DB":      ModuleDB,
+		"CPE":     ModuleCPE,
+	}[v]
+	if ok {
+		return &ret
 	}
 	return nil
 }
@@ -55,19 +52,17 @@ const (
 	OperationDelete = operation(3)
 )
 
-func parseOpString(v_p *string) *operation {
-	if v_p != nil {
-		var v = *v_p
-		var ret, ok = map[string]operation{
-			"C": OperationCreate,
-			"R": OperationRead,
-			"U": OperationUpdate,
-			"D": OperationDelete,
-		}[v]
-		if ok {
-			return &ret
-		}
+func parseOpString(v string) *operation {
+	var ret, ok = map[string]operation{
+		"C": OperationCreate,
+		"R": OperationRead,
+		"U": OperationUpdate,
+		"D": OperationDelete,
+	}[v]
+	if ok {
+		return &ret
 	}
+
 	return nil
 }
 
@@ -109,6 +104,12 @@ const (
 	MessageResponse = messageType(1)
 )
 
+const (
+	TOPIC_B_FORMAT     = `B/%s/%s`
+	TOPIC_U_REQ_FORMAT = `U/%s/%s/%s/%s/REQ/%s/%s`
+	TOPIC_U_RSP_FORMAT = `U/%s/%s/%s/%s/RSP/%s`
+)
+
 type Topic struct {
 	Dir            direction
 	SenderModule   module
@@ -126,22 +127,22 @@ func (self *Topic) ToString() *string {
 
 	switch self.Dir {
 	case DirectionBroadcast:
-		var v = fmt.Sprintf("B/%s/%s", sender_module, sender_id)
+		var v = fmt.Sprintf(TOPIC_B_FORMAT, sender_module, sender_id)
 		return &v
 	case DirectionUnicast:
 		if self.Type != nil && self.ReceiverModule != nil && self.ReceiverID != nil && self.RequestID != nil {
 			var t = *self.Type
-			var rmodule = *self.ReceiverModule
+			var rmodule = self.ReceiverModule.toString()
 			var rid = *self.ReceiverID
 			var reqid = *self.RequestID
 			switch t {
 			case MessageRequest:
 				if self.Operation != nil {
-					var v = fmt.Sprintf("U/%s/%s/%s/%s/REQ/%s/%s", sender_module, sender_id, rmodule, rid, reqid, self.Operation.toString())
+					var v = fmt.Sprintf(TOPIC_U_REQ_FORMAT, sender_module, sender_id, rmodule, rid, reqid, self.Operation.toString())
 					return &v
 				}
 			case MessageResponse:
-				var v = fmt.Sprintf("U/%s/%s/%s/%s/RSP/%s", sender_module, sender_id, rmodule, rid, reqid)
+				var v = fmt.Sprintf(TOPIC_U_RSP_FORMAT, sender_module, sender_id, rmodule, rid, reqid)
 				return &v
 			}
 		}
@@ -152,51 +153,37 @@ func (self *Topic) ToString() *string {
 
 func ParseTopic(topic_string string) *Topic {
 	if !strings.Contains(topic_string, " ") {
-		var dir_opt string
-		var smodule_s_opt string
-		var sid_opt string
-		var rmodule_opt string
-		var rid_opt string
-		var t_opt string
-		var reqid_opt string
-		var op_opt string
+		var dir_s string
+		var smodule_s string
+		var sid string
+		var rmodule_s string
+		var rid string
+		var msgtype_s string
+		var reqid string
+		var op_s string
 
-		fmt.Sscanf(strings.Replace(topic_string, "/", " ", -1), "%s %s %s %s %s %s %s %s", &dir_opt, &smodule_s_opt, &sid_opt, &rmodule_opt, &rid_opt, &t_opt, &reqid_opt, &op_opt)
+		fmt.Sscanf(strings.Replace(topic_string, "/", " ", -1), "%s %s %s %s %s %s %s %s", &dir_s, &smodule_s, &sid, &rmodule_s, &rid, &msgtype_s, &reqid, &op_s)
 
-		var smodule_opt = parseModuleString(&smodule_s_opt)
-		{
-			var dir_opt = map[bool]*string{true: &dir_opt, false: nil}[len(dir_opt) > 0]
-			var sid_opt = map[bool]*string{true: &sid_opt, false: nil}[len(sid_opt) > 0]
-			var rmodule_opt = map[bool]*string{true: &rmodule_opt, false: nil}[len(rmodule_opt) > 0]
-			var rid_opt = map[bool]*string{true: &rid_opt, false: nil}[len(rid_opt) > 0]
-			var t_opt = map[bool]*string{true: &t_opt, false: nil}[len(t_opt) > 0]
-			var reqid_opt = map[bool]*string{true: &reqid_opt, false: nil}[len(reqid_opt) > 0]
-			var op_opt = map[bool]*string{true: &op_opt, false: nil}[len(op_opt) > 0]
-			if dir_opt != nil && smodule_opt != nil && sid_opt != nil {
-				var dir = *dir_opt
-				var smodule = *smodule_opt
-				var sid = *sid_opt
-				if false {
-				} else if dir == "B" {
-					return &Topic{Dir: DirectionBroadcast, SenderModule: smodule, SenderID: sid}
-				} else if dir == "U" {
-					if t_opt != nil {
-						var uniType = *t_opt
-						if uniType == "REQ" || uniType == "RSP" {
-							var rmodule_opt = parseModuleString(rmodule_opt)
-							var op_opt = parseOpString(op_opt)
-							if false {
-							} else if uniType == "REQ" {
-								if rmodule_opt != nil && rid_opt != nil && reqid_opt != nil && op_opt != nil {
-									var t = MessageRequest
-									return &Topic{Dir: DirectionUnicast, SenderModule: smodule, SenderID: sid, ReceiverModule: rmodule_opt, ReceiverID: rid_opt, Type: &t, RequestID: reqid_opt, Operation: op_opt}
-								}
-							} else if uniType == "RSP" {
-								if rmodule_opt != nil && rid_opt != nil && reqid_opt != nil {
-									var t = MessageResponse
-									return &Topic{Dir: DirectionUnicast, SenderModule: smodule, SenderID: sid, ReceiverModule: rmodule_opt, ReceiverID: rid_opt, Type: &t, RequestID: reqid_opt}
-								}
+		var smodule_opt = parseModuleString(smodule_s)
+		if smodule_opt != nil {
+			var smodule = *smodule_opt
+			if false {
+			} else if dir_s == "B" {
+				return &Topic{Dir: DirectionBroadcast, SenderModule: smodule, SenderID: sid}
+			} else if dir_s == "U" {
+				if (msgtype_s == "REQ" || msgtype_s == "RSP") && len(reqid) > 0 {
+					var rmodule_opt = parseModuleString(rmodule_s)
+					if rmodule_opt != nil {
+						switch msgtype_s {
+						case "REQ":
+							var op_opt = parseOpString(op_s)
+							if op_opt != nil {
+								var t = MessageRequest
+								return &Topic{Dir: DirectionUnicast, SenderModule: smodule, SenderID: sid, ReceiverModule: rmodule_opt, ReceiverID: &rid, Type: &t, RequestID: &reqid, Operation: op_opt}
 							}
+						case "RSP":
+							var t = MessageResponse
+							return &Topic{Dir: DirectionUnicast, SenderModule: smodule, SenderID: sid, ReceiverModule: rmodule_opt, ReceiverID: &rid, Type: &t, RequestID: &reqid}
 						}
 					}
 				}
