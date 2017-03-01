bceddb1e5e49c3b92d48bc5c279deb072fa09c78
Artem Vorotnikov <artem@vorotnikov.me>
2017-03-01 13:48:05 +0300

    Fixed enum numeration

--- models.go
+++ models.go
@@ -7,16 +7,16 @@ type UUID string
 type SecurityType float64
 
 const (
-	WPA2Personal   = SecurityType(1)
-	WPA2Enterprise = SecurityType(2)
-	RADIUS         = SecurityType(3)
+	WPA2Personal   = SecurityType(0)
+	WPA2Enterprise = SecurityType(1)
+	RADIUS         = SecurityType(2)
 )
 
 type InterfaceType float64
 
 const (
-	I2_4 = InterfaceType(1)
-	I5_0 = InterfaceType(0)
+	I2_4 = InterfaceType(0)
+	I5_0 = InterfaceType(1)
 )
 
 type WLAN struct {
