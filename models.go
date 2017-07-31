94de59bcd9392d83756e6696f577ad40cfdb612a
Mr Jenkins <pgonchukov@wimarksystems.com>
2017-07-31 14:46:23 +0200

    WNE-377 #comment  change the marshal and unmarshall to system events

--- models.go
+++ models.go
@@ -349,21 +349,211 @@ type StatEventRule struct {
 
 // Events
 type SystemEvent struct {
-	Type       SystemEventObjectType
-	Data       interface{}      `bson:"data" json:"data"`
+	SystemEventObject
 	Timestamp  int64            `json:"timestamp"`
 	Subject_id string           `json:"subject_id"`
 	Level      SystemEventLevel `json:"level"`
 }
 
-func (se *SystemEvent) GetEvent() *SystemEventObject {
-	seo := &SystemEventObject{Data: se.Data, Type: se.Type}
-	return seo
+func (self SystemEvent) MarshalJSON() ([]byte, error) {
+	var b []byte
+	{
+		var err error
+		b, err = json.Marshal(self.SystemEventObject)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var doc Document
+	{
+		var err error
+		err = json.Unmarshal(b, &doc)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	doc["timestamp"] = self.Timestamp
+	doc["subject_id"] = self.Subject_id
+	doc["level"] = self.Level
+
+	return json.Marshal(doc)
 }
 
-func (se *SystemEvent) SetEvent(seo *SystemEventObject) {
-	se.Type = seo.Type
-	se.Data = seo.Data
+func (self *SystemEvent) UnmarshalJSON(b []byte) error {
+	var doc map[string]json.RawMessage
+	var err = json.Unmarshal(b, &doc)
+	if err != nil {
+		return err
+	}
+
+	if doc == nil {
+		return nil
+	}
+
+	// may be there is a way how to make code more reusable
+	// don't have time to figure out
+
+	var tsRaw, tsExists = doc["timestamp"]
+	if tsExists {
+		var ts int64
+		var tsErr = json.Unmarshal(tsRaw, &ts)
+		if tsErr == nil {
+			self.Timestamp = ts
+		} else {
+			return tsErr
+		}
+	}
+
+	delete(doc, "timestamp")
+
+	var subRaw, subExists = doc["subject_id"]
+	if subExists {
+		var subject_id string
+		var subErr = json.Unmarshal(subRaw, &subject_id)
+		if subErr == nil {
+			self.Subject_id = subject_id
+		} else {
+			return subErr
+		}
+	}
+
+	delete(doc, "subject_id")
+
+	var levelRaw, levelExists = doc["level"]
+	if levelExists {
+		var level SystemEventLevel
+		var levErr = json.Unmarshal(levelRaw, &level)
+		if levErr == nil {
+			self.Level = level
+		} else {
+			return levErr
+		}
+	}
+
+	delete(doc, "level")
+
+	var v, _ = json.Marshal(doc)
+
+	return self.SystemEventObject.UnmarshalJSON(v)
+}
+
+func (self *SystemEvent) GetBSON() (interface{}, error) {
+	var out = bson.M{}
+
+	levelBson, err := self.Level.GetBSON()
+	if err != nil {
+		return nil, err
+	}
+
+	var obj_b []byte
+	{
+		var err error
+		obj_b, err = bson.Marshal(self.SystemEventObject)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var obj bson.M
+	{
+		var err error
+		err = bson.Unmarshal(obj_b, &obj)
+		if err != nil {
+			return nil, err
+		}
+	}
+	out = obj
+	out["timestamp"] = self.Timestamp
+	out["subject_id"] = self.Subject_id
+	out["level"] = levelBson
+
+	return out, nil
+}
+
+func (self *SystemEvent) SetBSON(raw bson.Raw) error {
+	var in = bson.M{}
+	{
+		var err error
+		err = raw.Unmarshal(&in)
+		if err != nil {
+			return err
+		}
+	}
+
+	//timestamp
+	var v_timestamp, k_found = in["timestamp"]
+	if !k_found {
+		return errors.New("No timestamp found")
+	}
+
+	var obj_b []byte
+	{
+		var err error
+		obj_b, err = bson.Marshal(v_timestamp)
+		if err != nil {
+			return err
+		}
+	}
+	var err error
+	err = bson.Unmarshal(obj_b, &self.Timestamp)
+	if err != nil {
+		return err
+	}
+	delete(in, "timestamp")
+
+	//subject_id
+	v_subj, k_found := in["subject_id"]
+	if !k_found {
+		return errors.New("No subject found")
+	}
+
+	{
+		var err error
+		obj_b, err = bson.Marshal(v_subj)
+		if err != nil {
+			return err
+		}
+	}
+	err = bson.Unmarshal(obj_b, &self.Subject_id)
+	if err != nil {
+		return err
+	}
+	delete(in, "subject_id")
+
+	//subject_id
+	v_level, k_found := in["level"]
+	if !k_found {
+		return errors.New("No subject found")
+	}
+
+	{
+		var err error
+		obj_b, err = bson.Marshal(v_level)
+		if err != nil {
+			return err
+		}
+	}
+	err = bson.Unmarshal(obj_b, &self.Level)
+	if err != nil {
+		return err
+	}
+	delete(in, "level")
+
+	{
+		var err error
+		obj_b, err = bson.Marshal(in)
+		if err != nil {
+			return err
+		}
+	}
+	err = bson.Unmarshal(obj_b, &self)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type CPEConnectedData struct {
