9a0ba3b16a6f6e57fe9544d20ce14be15a34909d
Mr Jenkins <pgonchukov@wimarksystems.com>
2017-10-26 11:39:08 +0200

    change types from int to float32

--- back_scan.go
+++ back_scan.go
@@ -1,13 +1,13 @@
 package libwimark
 
 type BackScanModelRaw struct {
-	SSID       string `json:"ssid" bson:"ssid"`
-	BSSID      string `json:"bssid" bson:"bssid"`
-	Signal     int    `json:"signal" bson:"signal"`
-	Channel    int    `json:"channel" bson:"channel"`
-	Quality    int    `json:"quality" bson:"quality"`
-	QualityMax int    `json:"quality_max" bson:"quality_max"`
-	Mode       string `json:"mode" bson:"mode"`
+	SSID       string  `json:"ssid" bson:"ssid"`
+	BSSID      string  `json:"bssid" bson:"bssid"`
+	Signal     float32 `json:"signal" bson:"signal"`
+	Channel    int     `json:"channel" bson:"channel"`
+	Quality    float32 `json:"quality" bson:"quality"`
+	QualityMax int     `json:"quality_max" bson:"quality_max"`
+	Mode       string  `json:"mode" bson:"mode"`
 	Encryption struct {
 		Enabled      bool     `json:"enabled" bson:"enabled"`
 		AuthAlgs     []string `json:"auth_algs" bson:"auth_algs"`
