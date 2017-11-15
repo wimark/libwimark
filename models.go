0258d4bd2bc566324067b79959b4423d65b079ff
Mr Jenkins <pgonchukov@wimarksystems.com>
2017-11-15 15:31:20 +0100

    add description and name to template

--- models.go
+++ models.go
@@ -456,6 +456,7 @@ type L2TPTunnelSession struct {
 // CPE models
 
 type CPEConfigTemplate struct {
+	Description      string                `json:"description" bson:"description"`
 	Wifi             map[string]WiFiConfig `json:"wifi" bson:"wifi"`
 	LbsConfig        LBSConfig             `json:"lbs_config" bson:"lbs_config"`
 	StatisticsConfig StatisticsConfig      `json:"stats_config" bson:"stats_config"`
@@ -470,6 +471,7 @@ type CPEModel struct {
 }
 
 type ConfigRule struct {
+	Name     string `json:"name" bson:"name"`
 	Model    UUID   `json:"model" bson:"model"`
 	CPEs     []UUID `json:"cpes" bson:"cpes"`
 	Template struct {
