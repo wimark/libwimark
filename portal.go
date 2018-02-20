73451511e7fa0abca81c9c148092e06e40e2d632
Vadim Balakhanov <vbalakhanov@wimarksystems.com>
2018-02-20 16:31:25 +0300

    CaptiveRedirect model updated
    Signed-off-by: Vadim Balakhanov <vbalakhanov@wimarksystems.com>

--- portal.go
+++ portal.go
@@ -4,13 +4,17 @@ type GuestControlSettings struct {
 	CaptiveRedirect UUID `json:"captive_redirect" bson:"captive_redirect"`
 }
 
+type DnsAddress struct {
+	Ip         string `json:"ip" bson:"ip"`
+	DomainName string `json:"domain_name" bson:"domain_name"`
+}
+
 // struct for manage of cpe http-redirect system
 type CaptiveRedirect struct {
-	Name        string   `json:"name" bson:"name"`
-	RedirectURL string   `json:"redirect_url" bson:"redirect_url"`
-	MACs        []string `json:"mac_list" bson:"mac_list"`
-	URLs        []string `json:"url_list" bson:"url_list"`
-	SwitchURL   string   `json:"switch_url" bson:"switch_url"`
+	Name         string       `json:"name" bson:"name"`
+	RedirectURL  string       `json:"redirect_url" bson:"redirect_url"`
+	MACWhiteList []string     `json:"mac_list" bson:"mac_list"`
+	URLWhiteList []DnsAddress `json:"url_list" bson:"url_list"`
 }
 
 type PortalAuthRADIUS struct {
