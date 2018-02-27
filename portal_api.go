88990548f5e4ffd9625ea0208f90d4dc0a168d5f
Danya Sliusar <dsliusar@wimarksystems.com>
2018-02-27 16:26:42 +0300

    portal: some change in api for portal
    WNE-805 #time 10m

--- portal_api.go
+++ portal_api.go
@@ -7,11 +7,10 @@ import (
 // Struct for request payload from external captive portal
 
 type RedirectRequestObject struct {
-	Username string `json:"username" bson:"username" form:"username" query:"username" validate:"required"`
-	Password string `json:"password" bson:"password" form:"password" query:"password" validate:"required"`
-	MAC      string `json:"mac" bson:"mac" form:"mac" query:"mac" validate:"required,len=17"`
-	//Portal   string `json:"portal_id" bson:"portal_id" form:"portal_id" query:"portal_id" validate:"required"`
+	Username string `json:"username" bson:"username" form:"username" query:"username" validate:"-"`
+	Password string `json:"password" bson:"password" form:"password" query:"password" validate:"-"`
 
+	MAC  string `json:"mac" bson:"mac" form:"mac" query:"mac" validate:"required,len=17"`
 	CPE  string `json:"cpe_id" bson:"cpe_id" form:"cpe_id" query:"cpe_id" validate:"required"`
 	WLAN string `json:"wlan_id" bson:"wlan_id" form:"wlan_id" query:"wlan_id" validate:"required"`
 }
@@ -40,4 +39,5 @@ func (cv *Validator) Validate(i interface{}) error {
 type HTTPResponseObject struct {
 	Status      string `json:"status,omitempty"`
 	Description string `json:"description,omitempty"`
+	Code        string `json:"code,omitempty"`
 }
