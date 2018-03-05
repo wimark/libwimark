716ad18ce42b2a06bb4193d4d60906dfe588059f
Danya Sliusar <dsliusar@wimarksystems.com>
2018-03-05 19:53:44 +0300

    portal: change in json tag api for redirect req
    WNE-805 #time 10m

--- portal_api.go
+++ portal_api.go
@@ -14,7 +14,7 @@ type RedirectRequestObject struct {
 	CPE  string `json:"cpe_id" bson:"cpe_id" form:"cpe_id" query:"cpe_id" validate:"uuid"`
 	WLAN string `json:"wlan_id" bson:"wlan_id" form:"wlan_id" query:"wlan_id" validate:"uuid"`
 
-	Timeout int `json:"timeout,omitempty" bson:"timeout" form:"timeout" query:"timeout" validate:"-"`
+	Timeout int64 `json:"client_timeout,omitempty" bson:"client_timeout" form:"client_timeout" query:"client_timeout" validate:"-"`
 }
 
 // Struct for request payload from webui
@@ -51,17 +51,16 @@ type PortalClientSession struct {
 	WLAN     string `json:"wlan_id" bson:"wlan_id"`
 	CPE      string `json:"cpe_id" bson:"cpe_id"`
 	Redirect string `json:"redirect_id" bson:"redirect_id"`
+	Session  string `json:"session_id" bson:"session_id"`
 
-	AcctFirst int64 `json:"acct_first" bson:"acct_first"`
-	AcctLast  int64 `json:"acct_last" bson:"acct_last"`
+	AcctStart int64 `json:"acct_start" bson:"acct_start"`
+	AcctStop  int64 `json:"acct_stop" bson:"acct_stop"`
 	AuthStart int64 `json:"auth_start" bson:"auth_start"`
 	AuthStop  int64 `json:"auth_stop" bson:"auth_stop"`
-	Resolved  bool  `json:"resolved" bson:"resolved"`
 	Timeout   int64 `json:"timeout" bson:"timeout"`
 
-	Session     string `json:"session_id" bson:"session_id"`
-	RxBytes     int64  `json:"rx_bytes" bson:"rx_bytes"`
-	TxBytes     int64  `json:"tx_bytes" bson:"tx_bytes"`
-	RxBytesLast int64  `json:"rx_bytes_last" bson:"rx_bytes_last"`
-	TxBytesLast int64  `json:"tx_bytes_last" bson:"tx_bytes_last"`
+	RxBytes     int64 `json:"rx_bytes" bson:"rx_bytes"`
+	TxBytes     int64 `json:"tx_bytes" bson:"tx_bytes"`
+	RxBytesLast int64 `json:"rx_bytes_last" bson:"rx_bytes_last"`
+	TxBytesLast int64 `json:"tx_bytes_last" bson:"tx_bytes_last"`
 }
