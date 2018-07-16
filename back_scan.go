c2ccfe2fdc5ff7681949f7f85798471460aa0fa0
Vadim Balakhanov <vbalakhanov@wimarksystems.com>
2018-07-16 16:15:56 +0300

    Scandata updated: width & htmode
    Signed-off-by: Vadim Balakhanov <vbalakhanov@wimarksystems.com>

--- back_scan.go
+++ back_scan.go
@@ -1,14 +1,17 @@
 package libwimark
 
 type BackScanModelRaw struct {
-	SSID       string  `json:"ssid" bson:"ssid"`
-	BSSID      string  `json:"bssid" bson:"bssid"`
-	Signal     float32 `json:"signal" bson:"signal"`
-	Channel    int     `json:"channel" bson:"channel"`
-	Quality    float32 `json:"quality" bson:"quality"`
-	QualityMax int     `json:"quality_max" bson:"quality_max"`
-	Mode       string  `json:"mode" bson:"mode"`
-	Encryption struct {
+	SSID        string          `json:"ssid" bson:"ssid"`
+	BSSID       string          `json:"bssid" bson:"bssid"`
+	Signal      float32         `json:"signal" bson:"signal"`
+	Channel     int             `json:"channel" bson:"channel"`
+	Quality     float32         `json:"quality" bson:"quality"`
+	QualityMax  int             `json:"quality_max" bson:"quality_max"`
+	Mode        string          `json:"mode" bson:"mode"`
+	CenterIndex int             `json:"center_idx0" bson:"center_idx0"`
+	ChanOffset  int             `json:"sec_channel_offset" bson:"sec_channel_offset"`
+	HTModes     map[string]bool `json:"htmodelist" bson:"htmodelist"`
+	Encryption  struct {
 		Enabled      bool     `json:"enabled" bson:"enabled"`
 		AuthAlgs     []string `json:"auth_algs" bson:"auth_algs"`
 		Description  string   `json:"description" bson:"description"`
