3a6dfbc43c6d0a150cad1501714a88231eacb3a6
Daniil Sliusar <dsliusar@wimarksystems.com>
2018-11-14 12:09:08 +0300

    Add user classes for QoS from libtc
    WNE-1193

--- portal.go
+++ portal.go
@@ -1,5 +1,9 @@
 package libwimark
 
+import (
+	tc "bitbucket.org/wimarksystems/wbras/libtc"
+)
+
 type GuestControlSettings struct {
 	CaptiveRedirect UUID   `json:"captive_redirect" bson:"captive_redirect"`
 	MACAuth         []UUID `json:"mac_radius_auth_servers" bson:"mac_radius_auth_servers"`
@@ -16,4 +20,7 @@ type CaptiveRedirect struct {
 	RedirectURL  string       `json:"redirect_url" bson:"redirect_url"`
 	MACWhiteList []string     `json:"mac_list" bson:"mac_list"`
 	URLWhiteList []DnsAddress `json:"url_list" bson:"url_list"`
+
+	DefaultProfile string                  `json:"default_class" bson:"default_class"`
+	Profiles       map[string]tc.UserClass `json:"user_profiles" bson:"user_profiles"`
 }
