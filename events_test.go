1cb7ec58917d3b6aa16656775fb286e794337fde
Vadim Balakhanov <vbalakhanov@wimarksystems.com>
2018-12-26 17:29:21 +0300

    Event test fixed
    Signed-off-by: Vadim Balakhanov <vbalakhanov@wimarksystems.com>

--- events_test.go
+++ events_test.go
@@ -11,34 +11,42 @@ func TestParsingRRMEvent(t *testing.T) {
 			"cpes": [
 			  {
 				"cpe_id" : "643ba589-88af-4786-0000-f09fc2f3069e",
-				"channel" : 11,
-				"dbm" : 10
+				"channel" : {
+				  "channel" : 1
+				},
+				"power" : 10
 			  },
 			  {
 				"cpe_id" : "643ba589-88af-4786-1111-f09fc2f3069e",
-				"channel" : 7,
-				"dbm" : 20
+				"channel" : {
+				  "channel" : 7
+				},
+				"power" : 20
 			  }
 			]
 	    }
 	`)
 
 	var data RRMStatusData
-	json.Unmarshal(bt, &data)
+	e := json.Unmarshal(bt, &data)
+	if e != nil {
+		t.Error(e)
+	}
 
 	if l := len(data.Cpes); l != 2 {
 		t.Error()
+		return
 	}
 
 	if data.Cpes[0].CpeID != "643ba589-88af-4786-0000-f09fc2f3069e" {
 		t.Error()
 	}
 
-	if data.Cpes[1].Channel != 7 {
+	if data.Cpes[1].Channel.Channel != 7 {
 		t.Error()
 	}
 
-	if data.Cpes[1].Dbm != 20 {
+	if data.Cpes[1].Power != 20 {
 		t.Error()
 	}
 }
