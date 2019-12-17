361f0d4b3a9438000fe77542f047a0ecc47fb520
Daniil Sliusar <dsliusar@wimarksystems.com>
2019-12-17 21:40:28 +0300

    Add: stats and go mod
    WNE-1433

---
+++ colls.go
@@ -0,0 +1,16 @@
+package libwimark
+
+const (
+	CollCPEStat    = "stats"
+	CollClientStat = "client_stats"
+
+	CollCPEStatInfo    = "cpe_stat_info"
+	CollBSSStatInfo    = "bss_stat_info"
+	CollWLANStatInfo   = "wlan_stat_info"
+	CollClientStatInfo = "client_stat_info"
+
+	CollCPEStatInfoCache    = "cpe_stat_info_capped"
+	CollBSSStatInfoCache    = "bss_stat_info_capped"
+	CollWLANStatInfoCache   = "wlan_stat_info_capped"
+	CollClientStatInfoCache = "client_stat_info_capped"
+)
