ab53d0c4d63ab62fa8e31f39b8786fbd386b1728
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-08-06 18:44:22 +0400

    WNE-1845: Add: Delete LBSZoneMask

--- masks.go
+++ masks.go
@@ -75,21 +75,6 @@ type LBSCPEInfoMask struct {
 	Z     LimitBetweenOptional `json:"z"`
 }
 
-type LBSZoneMask struct {
-	SimpleMask
-	Name        string             `json:"name" bson:"name"`
-	Description string             `json:"description" bson:"description"`
-	Group       UUID               `json:"group"`
-	Corners     []CornerCoordsMask `json:"corners"`
-}
-
-type CornerCoordsMask struct {
-	SimpleMask
-	X LimitBetweenOptional `json:"x" bson:"x"`
-	Y LimitBetweenOptional `json:"y" bson:"y"`
-	Z LimitBetweenOptional `json:"z" bson:"z"`
-}
-
 type LBSClientCoordsMask struct {
 	TimestampMask
 	Group []UUID               `json:"group"`
