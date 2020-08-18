ff2ac7358da4e576f40ad7a77627bb8702b9e500
Danya Sliusar <dsliusar@wimarksystems.com>
2020-08-18 19:42:26 +0300

    WNE-1892: Change: redirect request move

--- redirect.go
+++ redirect.go
@@ -26,3 +26,101 @@ type CaptiveRedirect struct {
 
 	NoMasquerade bool `json:"no_masquerade" bson:"no_masquerade"`
 }
+
+// Platform part
+// --------
+
+// RedirectRequest struct for request payload from external captive portal
+type RedirectRequest struct {
+	// Basic and Needed client data
+	MAC  string `json:"mac" bson:"mac" form:"mac" query:"mac" validate:"required,mac"`
+	CPE  string `json:"cpe_id" bson:"cpe_id" form:"cpe_id" query:"cpe_id" validate:"uuid"`
+	WLAN string `json:"wlan_id" bson:"wlan_id" form:"wlan_id" query:"wlan_id" validate:"uuid"`
+
+	// Username will be use in RADIUS Acct
+	Username string `json:"username" bson:"username" form:"username" query:"username" validate:"-"`
+	Password string `json:"password" bson:"password" form:"password" query:"password" validate:"-"`
+
+	// passing NAS-ID to use in next RADIUS Acct
+	NasId string `json:"nas-id,omitempty" bson:"nas-id" form:"nas-id" query:"nas-id" validate:"-"`
+
+	// two way to set session-timeout
+	Timeout  int64 `json:"session-timeout,omitempty" bson:"session-timeout" form:"session-timeout" query:"session-timeout" validate:"-"`
+	Timeout2 int64 `json:"wimark-session-timeout,omitempty" bson:"wimark-session-timeout" validate:"-"`
+
+	// ACL group -- not using now
+	Group string `json:"wimark-client-group,omitempty" bson:"wimark-client-group" validate:"-"`
+
+	// Auth additional data
+	UserAgent string `json:"useragent"`
+
+	// auth type -- free / sponsor / paid / etc
+	AuthType string `json:"auth_type"`
+
+	// authentype -- sms / callback / etc
+	AuthenType string `json:"authen_type"`
+
+	// IP address of user -- if has
+	IP string `json:"ip" bson:"ip"`
+
+	// group if location was passed
+	GroupID string `json:"group-id"`
+}
+
+// RedirectClientSession struct for store redirect session on platform
+type RedirectClientSession struct {
+	ID string `json:"id" bson:"_id"`
+
+	Session   string `json:"session_id" bson:"session_id"`
+	MAC       string `json:"mac" bson:"mac"`
+	WLAN      string `json:"wlan_id" bson:"wlan_id"`
+	CPE       string `json:"cpe_id" bson:"cpe_id"`
+	Redirect  string `json:"redirect_id" bson:"redirect_id"`
+	Radio     string `json:"radio_id" bson:"radio_id"`
+	UserAgent string `json:"useragent" bson:"useragent"`
+	IP        string `json:"ip" bson:"ip"`
+
+	AcctStart int64 `json:"acct_start" bson:"acct_start"`
+	AuthStart int64 `json:"auth_start" bson:"auth_start"`
+	AuthStop  int64 `json:"auth_stop" bson:"auth_stop"`
+	Timeout   int64 `json:"timeout" bson:"timeout"`
+
+	// data from acct start
+	UserName         string `json:"User-Name"`
+	CallingStationId string `json:"Calling-Station-Id"`
+	CalledStationId  string `json:"Called-Station-Id"`
+	NasIdentifier    string `json:"NAS-Identifier"`
+	NasIPAddress     string `json:"NAS-IP-Address"`
+	FramedIPAddress  string `json:"Framed-IP-Address"`
+	UserQosGroup     string `json:"Qos-Group"`
+
+	// data for interim update and acct stop
+	AcctSessionTime     int `json:"Acct-Session-Time"`
+	AcctInputGigawords  int `json:"Acct-Input-Gigawords"`
+	AcctOutputGigawords int `json:"Acct-Output-Gigawords"`
+	AcctInputOctets     int `json:"Acct-Input-Octets"`
+	AcctOutputOctets    int `json:"Acct-Output-Octets"`
+	AcctInputPackets    int `json:"Acct-Input-Packets"`
+	AcctOutputPackets   int `json:"Acct-Output-Packets"`
+}
+
+// struct for update acct of redirect session on platform
+type RedirectClientSessionAcct struct {
+	AcctSessionTime     int `json:"Acct-Session-Time"`
+	AcctInputGigawords  int `json:"Acct-Input-Gigawords"`
+	AcctOutputGigawords int `json:"Acct-Output-Gigawords"`
+	AcctInputOctets     int `json:"Acct-Input-Octets"`
+	AcctOutputOctets    int `json:"Acct-Output-Octets"`
+	AcctInputPackets    int `json:"Acct-Input-Packets"`
+	AcctOutputPackets   int `json:"Acct-Output-Packets"`
+}
+
+type RedirectResponse struct {
+	Status string `json:"status,omitempty"`
+	Code   int    `json:"code"`
+
+	Description string      `json:"description,omitempty"`
+	Data        interface{} `json:"data,omitempty"`
+
+	State string `json:"state"`
+}
