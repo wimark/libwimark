3a079f4b604d90d0ae610b841b8df8969da61cc5
Vitaly Lisogubenko <vlisogubenko@wimark.com>
2021-08-13 16:47:37 +0400

    WNE-2390: Add: Add type RadarExportMacs for radar-export

--- util_radar.go
+++ util_radar.go
@@ -116,11 +116,14 @@ type AnalyticsMwHttpRequest struct {
 }
 
 func (r *AnalyticsMwHttpRequest) String() string {
-	s := fmt.Sprintf("start=%d&stop=%d&timeout=%d&period=%s&rate=%d&filter=%s&duration=%d&raw=%s&long=%s&hash=%s",
+	s := fmt.Sprintf(
+		"start=%d&stop=%d&timeout=%d&period=%s&rate=%d&filter=%s&duration=%d&raw=%s&long=%s&hash=%s&with_night_clients=%s&work_time_range=%szone=%s",
 		r.Start, r.Stop, r.Timeout, r.Period, r.Rate, r.Filter, r.Duration,
 		strconv.FormatBool(r.Raw),
 		strconv.FormatBool(r.Long),
-		strconv.FormatBool(r.Hash))
+		strconv.FormatBool(r.Hash),
+		strconv.FormatBool(r.WithNightClients),
+		r.WorkTimeRange, r.Zone)
 	if len(r.CPEs) != 0 {
 		cpes := strings.Join(r.CPEs, "&cpes[]=")
 		s = s + "&cpes[]=" + cpes
