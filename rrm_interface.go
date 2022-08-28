b5057a7b1bf3243ac77b6fa9dbee8d8915b3ff31
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2022-08-28 20:37:14 +0400

    WNE-2714 Add: RRMOperation type

--- rrm_interface.go
+++ rrm_interface.go
@@ -9,8 +9,9 @@ const (
 // JSONRPC parameters
 
 type RRMParamUpdateGroup struct {
-	Id    UUID     `json:"id"`
-	Group RRMGroup `json:"group"`
+	Id        UUID         `json:"id"`
+	Group     RRMGroup     `json:"group"`
+	Operation RRMOperation `json:"operation"`
 }
 
 // Algorithms parameters
@@ -23,3 +24,14 @@ type RRMGreedParams struct {
 	RRMTimerParams `bson:",inline"`
 	ManagePower    bool `json:"manage_power" bson:"manage_power"`
 }
+
+//go:generate enumer -type=RRMOperation -json -sql -transform=snake -output rrm_inteface_opeartion_string.go
+
+// RRMOperation -тип rrm операции
+type RRMOperation uint8
+
+const (
+	CreateRRMOperation RRMOperation = iota
+	UpdateRRMOperation
+	DeleteRRMOperation
+)
