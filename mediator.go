cb852359559cd518ccfda0313141c252d1660a8d
Daniil Sliusar <dsliusar@wimarksystems.com>
2023-01-12 13:52:08 +0400

    WNE-2947: Add: ExtAccessPoint for external access points

--- mediator.go
+++ mediator.go
@@ -1,20 +1,42 @@
 package libwimark
 
+// SSHAccess for ssh creds data
 type SSHAccess struct {
 	Username string `json:"username"`
 	Password string `json:"password,omitempty"`
 	Key      string `json:"key"`
+	Port     int    `json:"port"`
 }
 
+// Controller for integration with external controller (like Cisco)
 type Controller struct {
-	Status      ControllerStatusType `json:"status"`
-	Enable      bool                 `json:"enable"`
-	Name        string               `json:"name"`
-	Description string               `json:"description"`
-	MAC         string               `json:"mac"`
-	Serial      string               `json:"serial"`
-	Vendor      string               `json:"vendor"`
-	Firmware    string               `json:"fw_version"`
-	Access      SSHAccess            `json:"access"`
-	IPAddr      string               `json:"ip_addr"`
+	Status ControllerStatusType `json:"status"`
+	Enable bool                 `json:"enable"`
+
+	Name        string `json:"name"`
+	Description string `json:"description"`
+
+	MAC      string `json:"mac"`
+	Serial   string `json:"serial"`
+	Vendor   string `json:"vendor"`
+	Firmware string `json:"fw_version"`
+
+	IPAddr string    `json:"ip_addr"`
+	Access SSHAccess `json:"access"`
+}
+
+// ExtAccessPoint for represent External access point
+type ExtAccessPoint struct {
+	Status ConfigurationStatus `json:"status"`
+
+	Name        string `json:"name"`
+	Description string `json:"description"`
+
+	MAC      string `json:"mac"`
+	Serial   string `json:"serial"`
+	Vendor   string `json:"vendor"`
+	Firmware string `json:"fw_version"`
+
+	IPAddr string    `json:"ip_addr"`
+	Access SSHAccess `json:"access"`
 }
