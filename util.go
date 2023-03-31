18587b5b59d494bd7ad104f099c1e4ccdb0e2b7a
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2023-03-31 17:27:09 +0400

    DB-17 add util check wpaenterprise

--- util.go
+++ util.go
@@ -5,3 +5,15 @@ import "github.com/google/uuid"
 func NewUUID() string {
 	return uuid.New().String()
 }
+
+func IsWPAEnterprise(secType SecurityType) bool {
+	switch secType {
+	case SecurityTypeWPAEnterprise,
+		SecurityTypeWPA2Enterprise,
+		SecurityTypeWPA3Enterprise,
+		SecurityTypeWPA23Enterprise:
+		return true
+	default:
+		return false
+	}
+}
