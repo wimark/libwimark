274fb5a721f7a2bb9ad2198dfe78ef3b722f3ca8
Sergey Soldatov <ssacompany@mail.ru>
2023-08-15 19:08:54 +0300

    DB-269: Add: Troubleshooting filters
    Добавлены фильтры

--- troubleshooting.go
+++ troubleshooting.go
@@ -1,5 +1,17 @@
 package libwimark
 
-type RadioactiveLog struct {
-	State RadioActiveState `json:"state" bson:"state"`
+type Troubleshooting struct {
+	State                  RadioActiveState       `json:"state" bson:"state"`
+	TroubleshootingFilters TroubleshootingFilters `json:"troubleshooting_filters" bson:"troubleshooting_filters"`
+}
+
+type TroubleshootingFilters []TroubleshootingFilter
+
+type TroubleshootingFilter struct {
+	ID       string `json:"id" bson:"_id"`
+	MacIP    string `json:"mac_ip" bson:"mac_ip"`
+	FileName string `json:"filename" bson:"filename"`
+	URL      string `json:"url" bson:"url"`
+	StartAt  int64  `json:"start_at" bson:"start_at"`
+	StopAt   string `json:"stop_at" bson:"stop_at"`
 }
